4377e64cb306f7cd2302493d147203380e1b5c98
WojKoz4188 <49590251+PyronisSilnyKombi@users.noreply.github.com>
2021-01-22 19:28:42 +0100

    nagłówki light sensor i terminal

--- Core/Inc/light_sensor.h
+++ Core/Inc/light_sensor.h
@@ -8,4 +8,22 @@
 
 #include "i2c.h"
 
-void sensor_init(HAL_StatusTypeDef status, const uint8_t x1, const uint8_t x2, const uint8_t x3);
+#define BH1700_ADDRESS_L (0x23 << 1)
+#define BH1700_ADDRESS_H (0x5C << 1)
+#define BH1750_POWER_DOWN 				0x00
+#define BH1750_POWER_ON 				0x01
+#define BH1750_RESET 					0x07
+#define BH1750_CONTIONIUS_H_RES_MODE 	0x10
+#define BH1750_CONTIONIUS_H_RES_MODE2	0x11
+#define BH1750_CONTIONIUS_L_RES_MODE 	0x13
+#define BH1750_ONE_TIME_H_RES_MODE 		0x20
+#define BH1750_ONE_TIME_H_RES_MODE2		0x21
+#define BH1750_ONE_TIME_L_RES_MODE		0x23
+HAL_StatusTypeDef  BH1750_Status;
+
+int lux_int;
+uint8_t data[2];
+
+HAL_StatusTypeDef sensor_init();
+
+HAL_StatusTypeDef sensor_read();

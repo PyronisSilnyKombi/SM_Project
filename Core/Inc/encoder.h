c5aa33a900a9cf9bdd21f59d469627ccfb68988e
mmpiotrek <77836068+mmpiotrek@users.noreply.github.com>
2021-01-24 22:09:28 +0100

    H files final version

---
+++ Core/Inc/encoder.h
@@ -0,0 +1,18 @@
+/*
+ * encoder.h
+ *
+ *  Created on: Jan 23, 2021
+ *      Author: piotr
+ */
+
+#ifndef INC_ENCODER_H_
+#define INC_ENCODER_H_
+
+int old_count;
+int user_val_encoder;
+
+int encoder_count(int count);
+
+
+
+#endif /* INC_ENCODER_H_ */
--- Core/Inc/terminal_comunication.h
+++ Core/Inc/terminal_comunication.h
@@ -9,6 +9,10 @@
 
 char rx_buffer[15];
 int received_val;
+int trial_transmission;
+int user_val_terminal;
+_Bool flag;
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart);
 void terminal_message(int lux_int);
+int terminal_value();
